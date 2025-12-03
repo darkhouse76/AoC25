4ccb8e6179c6aa394315258f5b8a3edad4d066ea
darkhouse76 <150859706+darkhouse76@users.noreply.github.com>
2025-12-03 03:58:19 -0800

    Start of day 3

---
+++ Day3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC25
+{
+    internal static class Day3
+    {
+        public static void Run(int part, string input) {
+            switch (part) {
+                case 1:
+                    Part1(input);
+                    break;
+                case 2:
+                    Part2(input);
+                    break;
+            }
+        }
+
+        public static void Part1(string input) {
+
+
+        }
+
+        public static void Part2(string input) {
+
+
+        }
+
+    }
+}
